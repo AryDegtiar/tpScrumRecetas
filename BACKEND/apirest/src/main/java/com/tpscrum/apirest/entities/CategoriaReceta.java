79564a319fd192061dcb9376d9f4d20fc9f709a0
tgriffabenitez <tgriffabenitez@gmail.com>
2023-03-30 18:57:59 -0300

    Se genero la entidad, controller, service y repository para la categoria de las recetas

---
+++ BACKEND/apirest/src/main/java/com/tpscrum/apirest/entities/CategoriaReceta.java
@@ -0,0 +1,24 @@
+package com.tpscrum.apirest.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "categoria_receta")
+public class CategoriaReceta extends Base{
+
+    @NotBlank(message = "El nombre no puede estar vacío")
+    private String nombre;
+
+    @NotBlank(message = "La descripción no puede estar vacía")
+    private String descripcion;
+}
