683c3ddb315bf9d775e4c0d769b3670fad3a518f
AryDegtiar <adegtiar@frba.utn.edu.ar>
2023-04-03 11:32:13 -0300

    se modifica el filtro de ingredientes en la tabla de ingredientes

--- BACKEND/apirest/src/main/java/com/tpscrum/apirest/controllers/IngredienteController.java
+++ BACKEND/apirest/src/main/java/com/tpscrum/apirest/controllers/IngredienteController.java
@@ -18,10 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class IngredienteController extends BaseControllerImpl<Ingrediente, IngredienteServiceImpl>{
     @Autowired
     private IngredienteServiceImpl ingredienteServiceImpl;
-    
+
+
     @GetMapping("/search")
-    public ResponseEntity<?> search(@RequestParam String filter, Pageable pageable){
+    public ResponseEntity<?> search(@RequestParam(name = "filtrar", required = false) String filter, Pageable pageable){
         try {
+            if (filter == null)
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(("{\"error\": \"Debe ingresar un filtro\"}"));
+
             return ResponseEntity.status(HttpStatus.OK).body(ingredienteServiceImpl.search(filter, pageable));
         }catch ( Exception e){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(("{\"error\": \"" + e.getMessage() + "\"}"));
--- BACKEND/apirest/src/main/java/com/tpscrum/apirest/repositories/IngredienteRepository.java
+++ BACKEND/apirest/src/main/java/com/tpscrum/apirest/repositories/IngredienteRepository.java
@@ -9,10 +9,15 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IngredienteRepository extends BaseRepository<Ingrediente, Integer>{
+
+    @Query(value = "SELECT a FROM Ingrediente a WHERE UPPER(a.nombre) = UPPER(:filter)")
+    Page<Ingrediente> search (@Param("filter") String filter, Pageable pageable);
+
+    /*
     @Query(value = "SELECT a FROM Ingrediente a WHERE a.nombre LIKE %:filter% OR a.descripcion LIKE %:filter%")
     Page<Ingrediente> search (@Param("filter") String filter, Pageable pageable);
     
-    /*@Query (
+    @Query (
         value = "SELECT * FROM ingrediente WHERE nombre LIKE %:filter% OR descripcion LIKE %:filter%",
         countQuery = "SELECT count(*) FROM ingrediente",
         nativeQuery = true
