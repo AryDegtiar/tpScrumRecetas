5bc289bb62e16ea4f4691e1c1a5c1ff2b0ec552c
Nicolas Preiti <nicolaspreiti45@gmail.com>
2023-04-03 19:16:48 -0300

    recipes created

--- FRONTEND/src/components/Recipes.jsx
+++ FRONTEND/src/components/Recipes.jsx
@@ -1,59 +1,53 @@
-import { useState } from "react"
-
-const recipes3 = [
-  {
-    name: "pollo al horno con papas",
-    description: "receta vegana para comer en 2hs",
-    ingredients: ["papas", "pollo", "cebolla", "aji"],
-  },
-  {
-    name: "pollo al horno con papas",
-    description: "receta vegana para comer en 2hs",
-    ingredients: ["papas", "pollo", "cebolla", "aji"],
-  },
-  {
-    name: "pollo al horno con papas",
-    description: "receta vegana para comer en 2hs",
-    ingredients: ["papas", "pollo", "cebolla", "aji"],
-  },
-  {
-    name: "pollo al horno con papas",
-    description: "receta vegana para comer en 2hs",
-    ingredients: ["papas", "pollo", "cebolla", "aji"],
-  },
-]
+import { useEffect, useState } from "react"
+import { getRecipes } from "../services/recipes.service"
 
 export function Recipes() {
-  const [recipes, setRecipes] = useState(recipes3)
+  const [recipes, setRecipes] = useState([])
+
+  useEffect(() => {
+    async function effect() {
+      const response = await getRecipes()
+      setRecipes(response)
+    }
+
+    effect()
+  }, [])
+
+  console.log(recipes)
 
   return (
     <div className="flex flex-col items-center gap-2 m-10 p-4 rounded-lg bg-gray-100">
       <h1 className="font-primaryT font-black uppercase text-4xl text-black mb-4">Recetas populares</h1>
       <div className="flex flex-wrap gap-3 justify-center">
-        {recipes.map(({name, ingredients, description}) => (
-          <Recipe name={name} ingredients={ingredients} description={description}/>
+        {recipes.map(({nombre, descripcion, imagen, ingredientes}) => (
+          <Recipe name={nombre} description={descripcion} image={imagen} ingredients={ingredientes}/>
         ))}
       </div>
     </div>
   )
 }
 
-export function Recipe({name, description, ingredients}) {
+export function Recipe({name, description, image, ingredients}) {
   return (
-    <div className="flex flex-col cursor-pointer min-w-[260px] min-h-[130px] p-3 rounded-md bg-gradient-to-r from-primaryT from-90% to-transparent hover:shadow-md hover:scale-105 ease-in duration-100">
-      <h4 className="font-primaryT capitalize text-md font-bold text-white mb-3">{name}</h4>
-      <p className="font-primaryT capitalize font-semibold text-xs text-black">
-        {description}
-      </p>
-      <div className="flex gap-1 font-primaryT text-xs text-black">
-        <span className="capitalize font-black">
-          Ingredientes:
-        </span>
-        <p className="capitalize font-normal">
-          {ingredients.map((i) => (
-            i + "-"
-          ))}
+    <div className={`relative flex flex-col cursor-pointer min-w-[260px] min-h-[130px] p-3 py-4 rounded-md bg-gradient-to-r from-primaryT from-90% to-transparent hover:shadow-md hover:scale-105 ease-in duration-100 overflow-hidden`}>
+      <div className="z-10">
+        <h4 className="font-primaryT capitalize text-md font-bold text-white leading-4 drop-shadow-md shadow-[blue]">{name}</h4>
+        <p className="font-primaryT italic font-semibold text-xs text-gray-600">
+          {description}
         </p>
+        <div className="flex gap-1 font-primaryT text-xs text-gray-100">
+          <span className="font-black capitalize">Ingredientes:</span>
+          <p className="font-bold text-gray-100">
+            {
+              ingredients.map(i => (
+                i.nombre + "-"
+              ))
+            }
+          </p>
+        </div>
+      </div>
+      <div className="z-0 justify-end absolute top-0 left-0 flex w-full h-full overflow-hidden">
+        <img src={image} alt={name} className="w-52 h-full object-cover rounded-full translate-x-14" />
       </div>
     </div>
   )
